fd7038a961952c5329d1aebeb6b5bf7714197641
Martin Harník <127622021+martinharnik@users.noreply.github.com>
2024-06-13 21:54:56 +0200

    Add files via upload

--- discount_simulation.py
+++ discount_simulation.py
@@ -71,11 +71,11 @@ If you are not sure what to input, hover over the little help buttons next to ea
 
 # Form for input parameters
 with st.form(key='predict_form'):
-    sales_district = st.selectbox("Sales District", options=[""] + ["Northern Europe", "Central America", "Southeast Asia", "Eastern Africa", "Western Australia"], index=0, format_func=lambda x: 'Choose an option' if x == "" else x, help="Select the Sales District from the list")
-    customer_name = st.selectbox("Customer Name", options=[""] + ["GlobalTech Solutions", "Innovatech Industries", "GreenWave Enterprises", "NexaCorp International", "FutureLink Systems"], index=0, format_func=lambda x: 'Choose an option' if x == "" else x, help="Select the Customer Name from the list")
-    product_category = st.selectbox("Product Category", options=[""] + ["Energy", "Healthcare", "Industrial Automation", "Mobility", "Digital Industries"], index=0, format_func=lambda x: 'Choose an option' if x == "" else x, help="Select the Product Category from the list")
+    sales_district = st.selectbox("Sales District", options=[""] + ["Europe", "Central America", "Southeast Asia", "Eastern Africa", "Western Australia"], index=0, format_func=lambda x: 'Choose an option' if x == "" else x, help="Select the Sales District from the list")
+    customer_name = st.selectbox("Customer Name", options=[""] + ["České dráhy, a.s.", "ČEZ a.s.", "ČKD Group", "Letiště Praha, a.s.", "Škoda Auto a.s."], index=0, format_func=lambda x: 'Choose an option' if x == "" else x, help="Select the Customer Name from the list")
+    product_category = st.selectbox("Product Category", options=[""] + ["Energy", "Industrial Automation", "Mobility", "Digital Industries"], index=0, format_func=lambda x: 'Choose an option' if x == "" else x, help="Select the Product Category from the list")
     product_sub_category = st.selectbox("Product Sub-Category", options=[""] + ["Robotics", "Motion Control", "Industrial Ethernet"], index=0, format_func=lambda x: 'Choose an option' if x == "" else x, help="Select the Product Sub-Category from the list")
-    item_name = st.selectbox("Item Name", options=[""] + ["Assembly Line", "Solar Power System", "Digital X-Ray Machine", "Electric Vehicle Charging Station"], index=0, format_func=lambda x: 'Choose an option' if x == "" else x, help="Select the Item Name from the list")
+    item_name = st.selectbox("Item Name", options=[""] + ["Assembly Line", "Solar Power System", "Electric Vehicle Charging Station"], index=0, format_func=lambda x: 'Choose an option' if x == "" else x, help="Select the Item Name from the list")
     item_count = st.number_input("Item Count", min_value=0, value=0, format='%d', step=1, key='item_count', help="Enter the total number of items in the order")
     
     # Calculate Optimal Discount button
@@ -90,5 +90,5 @@ if predict_button:
     discounted_price = item_price * (1 - optimal_discount / 100)
     
     st.write(f"### Item Price (EUR): {item_price:.2f}")
-    st.write(f"### Optimal Discount: {optimal_discount:.2f} %")
+    st.write(f"### Optimal Discount: {optimal_discount:.2f}%")
     st.write(f"### Discounted Price (EUR): {discounted_price:.2f}")
